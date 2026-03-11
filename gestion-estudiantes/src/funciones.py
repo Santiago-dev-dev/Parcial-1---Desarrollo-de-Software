4c865ab66b0859bd456ab11b473f7d51e1d40646
mebaez <saacuna83@ucatolica.edu.co>
2026-03-11 12:42:25 -0500

    se realiza mejora

--- gestion-estudiantes/src/funciones.py
+++ gestion-estudiantes/src/funciones.py
@@ -1,6 +1,15 @@
 # src/funciones.py
 
 def hallar_maxima_nota(notas):
-    if not notas:  
-        return None  
-    return max(notas) 
+    if not notas:
+        return None  # Si la lista está vacía, retornamos None
+    
+   
+    notas_validas = [nota for nota in notas if isinstance(nota, (int, float)) and 0 <= nota <= 5]
+    
+ 
+    if not notas_validas:
+        return None
+
+    
+    return max(notas_validas)
