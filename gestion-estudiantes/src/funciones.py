eee325fba933bd4e3794055711852fe2e691a7b9
mebaez <saacuna83@ucatolica.edu.co>
2026-03-11 12:28:56 -0500

    se agrega funciones

--- gestion-estudiantes/src/funciones.py
+++ gestion-estudiantes/src/funciones.py
@@ -1,4 +1,4 @@
 def maxima_nota(notas):
-    if not notas:  # Si la lista está vacía, devolvemos 0 o un mensaje adecuado
+    if not notas:  
         return 0
-    return max(notas)  # Usamos la función max para obtener la máxima nota
+    return max(notas)  
