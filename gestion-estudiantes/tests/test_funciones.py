1762876b7dd8c128f9c51728174dd36d1120c591
Strujillo68 <strujillo68@ucatolica.edu.co>
2026-03-13 12:01:04 -0500

    Update test_funciones.py
    se actualiza

--- gestion-estudiantes/tests/test_funciones.py
+++ gestion-estudiantes/tests/test_funciones.py
@@ -5,13 +5,13 @@ from src.funciones import hallar_maxima_nota
 class TestFunciones(unittest.TestCase):
 
     def test_hallar_maxima_nota(self):
-        # Caso con notas válidas
+        
         self.assertEqual(hallar_maxima_nota([3.5, 4.2, 5.0]), 5.0)
         
-        # Caso con lista vacía
+        
         self.assertEqual(hallar_maxima_nota([]), None)
         
-        # Caso con notas entre 0 y 5
+        
         self.assertEqual(hallar_maxima_nota([0, 1, 2, 4, 5]), 5)
 
 if __name__ == "__main__":
